fab6df7cb9b217aac1c88ca094a9d7a5e38f193f
yashsalvi <47267252+yashsalvi@users.noreply.github.com>
2021-04-02 18:26:11 +0530

    Update admin.js

--- app/http/middlewares/admin.js
+++ app/http/middlewares/admin.js
@@ -1,8 +1,8 @@
 function admin (req, res, next) {
     if(req.isAuthenticated() && req.user.role === 'admin') {
-        return next()
+        return next();
     }
-    return res.redirect('/')
+    return res.redirect('/');
 }
 
 module.exports = admin
