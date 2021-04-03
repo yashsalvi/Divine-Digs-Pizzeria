381c91526935ea5a05832a627f751c13e5621510
yashsalvi <47267252+yashsalvi@users.noreply.github.com>
2021-04-03 19:03:31 +0530

    Updated the eventEmitter

--- app/http/controllers/admin/statusController.js
+++ app/http/controllers/admin/statusController.js
@@ -7,6 +7,7 @@ function statusController() {
                 if(err) {
                     return res.redirect('/admin/orders')
                 }
+                
                 // Emit event 
                 const eventEmitter = req.app.get('eventEmitter')
                 eventEmitter.emit('orderUpdated', { id: req.body.orderId, status: req.body.status })
