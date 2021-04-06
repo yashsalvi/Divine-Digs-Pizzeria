03eb63c0acf9d884b704e96e71cc7d3dc04dafb8
yashsalvi <47267252+yashsalvi@users.noreply.github.com>
2021-04-06 18:54:55 +0530

    Update admin.js

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 import moment from 'moment'
 import Noty from 'noty'
-
 export function initAdmin(socket) {
     const orderTableBody = document.querySelector('#orderTableBody')
     let orders = []
